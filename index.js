747c983b85714f576b1e4ad90c395c435ed4a0f1
JustelioCode <krutikovasjustinas@gmail.com>
2025-11-28 12:43:09 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+let megstamosPrekes = [];
+
+function parodytiIvestaReiksme() {
+	let ivestiesLaukelis = document.querySelector("#megstamos-prekes-ivestis");
+	let reiksme = ivestiesLaukelis.value;
+	let sarasas = document.querySelector("#megstamos-prekes");
+	megstamosPrekes.push(reiksme);
+	sarasas.innerHTML = sarasas.innerHTML + "<li>" + reiksme + "</li>";
+	console.log(megstamosPrekes[1]);
+}
